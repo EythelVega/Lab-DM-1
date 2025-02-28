d1133f6357ddecbe4cfe1437ac75b45f9fbc24b1
Gave <eythel.vegag@uanl.edu.mx>
2025-02-28 14:59:15 -0600

    Diseno de splashcreen con logo

--- LAB INGDISPMOV/app/src/main/java/com/bav/labdispositivosmovilesbav/SplashScreen.kt
+++ LAB INGDISPMOV/app/src/main/java/com/bav/labdispositivosmovilesbav/SplashScreen.kt
@@ -1,22 +1,26 @@
 package com.bav.labdispositivosmovilesbav
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    // Retraso de 2 segundos antes de ir a la pantalla de login
+    // Retraso antes de navegar a la pantalla de inicio
     LaunchedEffect(Unit) {
         delay(2000)
-        navController.navigate("login") { // Cambia "login" para ir a la pantalla de inicio
+        navController.navigate("login") { // Cambia "login" si quieres ir a otra pantalla
             popUpTo("splash") { inclusive = true }
         }
     }
@@ -24,14 +28,27 @@ fun SplashScreen(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF6200EE)), // Color de fondo
+            .background(Color(0xFF0E181E)), // Personaliza el color de fondo si lo deseas
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "KAYID",
-            color = Color.White,
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Agregar el texto "KAYID"
+            Text(
+                text = "KAYID",
+                color = Color.White,
+                fontSize = 75.sp,
+                fontWeight = FontWeight.Bold
+
+            )
+            Spacer(modifier = Modifier.height(5.dp)) // Espacio entre el texto y la imagen
+            // Mostrar la imagen del logo
+            Image(
+                painter = painterResource(id = R.drawable.mi_logo_foreground), // Asegúrate de que el nombre coincide
+                contentDescription = "Logo de la app",
+                modifier = Modifier.size(300.dp) // Ajusta el tamaño si es necesario
+            )
+        }
     }
 }
